0d8af5b04b81b4f85f443c5a8462ddc3389b1364
Mr. Patrick <carbon2320@gmail.com>
2023-02-27 10:32:51 +0300

    proxy server & client added (#3)
    docker files added
    docker compose file added
    Readme updated

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,17 @@
 FROM golang:1.20-alpine
 
 WORKDIR /bot
-#ENV TELEGRAM_APITOKEN=tkn
+#ENV TELEGRAM_APITOKEN=!BotFather-tkn!
+#ENV SERVER_ADDR=localhost:50051
 
 COPY go.mod ./
 COPY go.sum ./
 
 RUN go mod download
 
-COPY *.go ./
+COPY bot.go ./
+COPY proxyClient.go ./
+COPY proto ./proto
 
 RUN go build -o /tg-bot
 
---
+++ proxyClient.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"flag"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"log"
+	"os"
+	pb "tg-bot/proto"
+)
+
+var (
+	addr = flag.String("addr",
+		getEnvOrDefault("SERVER_ADDR", "localhost:50051"),
+		"the address to connect to")
+)
+
+func getEnvOrDefault(key string, def string) string {
+	if value, ok := os.LookupEnv(key); ok {
+		return value
+	}
+	return def
+}
+
+func CreateProxyClient() pb.ProxyClient {
+	flag.Parse()
+	// Set up a connection to the server.
+	log.Printf("Connecting to addr: %v", *addr)
+	conn, err := grpc.Dial(*addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	//defer conn.Close()
+	cl := pb.NewProxyClient(conn)
+	return cl
+}
