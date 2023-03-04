049aef0ac7f4e2baf8033c00c39d0ddd9af8cf51
Patrick <carbon2320@gmail.com>
2023-03-04 19:35:42 +0300

    callback added

--- bot.go
+++ bot.go
@@ -17,43 +17,122 @@ func main() {
 	if err != nil {
 		log.Panic(err)
 	}
-
 	bot.Debug = true
-
 	log.Printf("Authorized on account %s\n", bot.Self.UserName)
 
 	u := api.NewUpdate(0)
 	u.Timeout = 60
-
 	updates := bot.GetUpdatesChan(u)
 
 	for update := range updates {
-		if update.Message != nil { // If we got a message
-			log.Printf("[%s] %s\n", update.Message.From.UserName, update.Message.Text)
+		fmt.Println(update)
+		processUpdateMessage(bot, update)
+		processCallbackQuery(bot, update)
+	}
+}
 
-			serverMessage, err := process(update.Message.Text)
+func processCallbackQuery(bot *api.BotAPI, update api.Update) {
+	if update.CallbackQuery != nil {
+		// Respond to the callback query, telling Telegram to show the user
+		// a message with the data received.
 
-			if err != nil {
-				sendErrorMessage(bot, update, err)
-				continue
-			}
-
-			sendMsg(bot, update, serverMessage)
+		callback := api.NewCallback(update.CallbackQuery.ID, update.CallbackQuery.Data)
+		if _, err := bot.Request(callback); err != nil {
+			panic(err)
 		}
+		response, err := processCallback(update)
+
+		if err != nil {
+			sendErrorMessage(bot, update, err)
+			return
+		}
+		editMsg(bot, update, response)
+	}
+}
+
+func processUpdateMessage(bot *api.BotAPI, update api.Update) {
+	if update.Message != nil {
+		log.Printf("[%s] %s\n", update.Message.From.UserName, update.Message.Text)
+		serverMessage, err := process(update)
+
+		if err != nil {
+			sendErrorMessage(bot, update, err)
+			return
+		}
+		sendMsg(bot, update, serverMessage)
+	}
+}
+
+func createInlineKeyboard(keyboard *pb.Keyboard) api.InlineKeyboardMarkup {
+	kb := api.NewInlineKeyboardMarkup()
+
+	for _, row := range keyboard.GetRows() {
+		rw := api.NewInlineKeyboardRow()
+		for _, button := range row.GetButtons() {
+			button := api.NewInlineKeyboardButtonData(button.GetName(), button.GetValue())
+			rw = append(rw, button)
+		}
+		kb.InlineKeyboard = append(kb.InlineKeyboard, rw)
+	}
+	return api.NewInlineKeyboardMarkup(kb.InlineKeyboard...)
+}
+
+func createInlineKeyboardLink(keyboard *pb.Keyboard) *api.InlineKeyboardMarkup {
+	kb := createInlineKeyboard(keyboard)
+	return &api.InlineKeyboardMarkup{
+		InlineKeyboard: kb.InlineKeyboard,
 	}
 }
 
 func sendMsg(bot *api.BotAPI, update api.Update, serverMessage *pb.ProxyResponse) {
-
 	message := serverMessage.GetMessage()
 	length := len(message)
 	var msgs []string
 
 	msgs = splitLongLengthMessages(length, msgs, message)
 
-	for _, m := range msgs {
+	for id, m := range msgs {
 		msg := api.NewMessage(update.Message.Chat.ID, m)
 		msg.ReplyToMessageID = update.Message.MessageID
+		applyToOnlyTheFirstMessage := id == 0
+
+		if serverMessage.ApplyReplyMarkupKeyboard && applyToOnlyTheFirstMessage {
+			msg.ReplyMarkup = createInlineKeyboard(serverMessage.GetReplyMarkupKeyboard())
+		}
+
+		if serverMessage.ApplyMarkdownV2 {
+			msg.ParseMode = "MarkdownV2"
+		}
+
+		send, err := bot.Send(msg)
+		log.Println(send)
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+	}
+}
+
+func editMsg(bot *api.BotAPI, update api.Update, serverMessage *pb.ProxyResponse) {
+
+	message := serverMessage.GetMessage()
+	length := len(message)
+	var msgs []string
+	msgs = splitLongLengthMessages(length, msgs, message)
+
+	for id, m := range msgs {
+		msg := api.NewEditMessageText(
+			update.CallbackQuery.Message.Chat.ID,
+			update.CallbackQuery.Message.MessageID,
+			m)
+
+		applyToOnlyTheFirstMessage := id == 0
+
+		if serverMessage.ApplyReplyMarkupKeyboard && applyToOnlyTheFirstMessage {
+			msg.ReplyMarkup = createInlineKeyboardLink(serverMessage.GetReplyMarkupKeyboard())
+		} else if applyToOnlyTheFirstMessage {
+			msg.ReplyMarkup = update.CallbackQuery.Message.ReplyMarkup
+		}
 
 		if serverMessage.ApplyMarkdownV2 {
 			msg.ParseMode = "MarkdownV2"
@@ -107,14 +186,45 @@ func sendErrorMessage(bot *api.BotAPI, update api.Update, err error) {
 	}
 }
 
-func process(msg string) (response *pb.ProxyResponse, err error) {
+func process(update api.Update) (response *pb.ProxyResponse, err error) {
 	client := CreateProxyClient()
 	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
 	defer cancel()
-	response, err = client.Process(ctx, &pb.ProxyRequest{Message: msg})
+
+	response, err = client.Process(ctx, &pb.ProxyRequest{
+		Message:   update.Message.Text,
+		Username:  update.Message.From.UserName,
+		MessageID: int64(update.Message.MessageID),
+		ChatID:    update.Message.Chat.ID,
+	})
+
 	if err != nil {
 		log.Printf("could not process: %v", err)
-		err = fmt.Errorf("Can't process msg:%s, err:%v", msg, err)
+		err = fmt.Errorf("can't process msg:%s, err:%v", update.Message.Text, err)
+		return
+	}
+	return
+}
+
+func processCallback(update api.Update) (response *pb.ProxyResponse, err error) {
+	client := CreateProxyClient()
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	message := update.CallbackQuery.Message
+	var sourceUserMessage = message.ReplyToMessage.Text
+
+	response, err = client.Process(ctx, &pb.ProxyRequest{
+		Message:      sourceUserMessage,
+		MessageID:    int64(message.ReplyToMessage.MessageID),
+		Username:     message.From.UserName,
+		ChatID:       message.ReplyToMessage.Chat.ID,
+		CallbackData: update.CallbackQuery.Data,
+	})
+
+	if err != nil {
+		log.Printf("could not process: %v", err)
+		err = fmt.Errorf("can't process msg:%s, err:%v", update.Message.Text, err)
 		return
 	}
 	return
